2e72bae6e766f53e3c365e4820862b0074e5ba4c
infinitie <infinitiecity@gmail.com>
2026-03-24 23:11:51 +0300

    creating proper match cases for the scanner

--- scanner.py
+++ scanner.py
@@ -30,50 +30,49 @@ class Scanner:
     
     # Recognizing lexemes and operators
     def scanToken(self):
-        case = self.advance()
-        if case == "(":
-            self.addToken(TokenType.LEFT_PAREN)
-        elif case == ")":
-            self.addToken(TokenType.RIGHT_PAREN)
-        elif case == "{":
-            self.addToken(TokenType.LEFT_BRACE)
-        elif case == "}":
-            self.addToken(TokenType.RIGHT_BRACE)
-        elif case == ",":
-            self.addToken(TokenType.COMMA)
-        elif case == ".":
-            self.addToken(TokenType.DOT)
-        elif case == "-":
-            self.addToken(TokenType.MINUS)
-        elif case == "+":
-            self.addToken(TokenType.PLUS)
-        elif case == ";":
-            self.addToken(TokenType.SEMICOLON)
-        elif case == "*":
-            self.addToken(TokenType.STAR)
-        elif case == "!":
-            self.addToken(TokenType.BANG)
-
-        # Operators with two characters
-        elif case == "=":
-            self.addToken(TokenType.EQUAL_EQUAL if self.match("=") else TokenType.EQUAL)
-
-        elif case == "<":
-            self.addToken(TokenType.LESS_EQUAL if self.match("=") else TokenType.LESS)
+        char = self.advance()
         
-        elif case == ">":
-            self.addToken(TokenType.GREATER_EQUAL if self.match("=") else TokenType.GREATER)
+        match char:
+            # Single-character tokens
+            case "(": self.addToken(TokenType.LEFT_PAREN)
+            case ")": self.addToken(TokenType.RIGHT_PAREN)
+            case "{": self.addToken(TokenType.LEFT_BRACE)
+            case "}": self.addToken(TokenType.RIGHT_BRACE)
+            case ",": self.addToken(TokenType.COMMA)
+            case ".": self.addToken(TokenType.DOT)
+            case "-": self.addToken(TokenType.MINUS)
+            case "+": self.addToken(TokenType.PLUS)
+            case ";": self.addToken(TokenType.SEMICOLON)
+            case "*": self.addToken(TokenType.STAR)
 
-        elif case == "/":
-            if self.match("/"):
-                while self.peek() != "\n" and self.isAtEnd():
-                    self.advance()
-            else:
-                self.addToken(TokenType.SLASH)
-            
+            # Operator tokens (one or two characters)
+            case "!":
+                self.addToken(TokenType.BANG_EQUAL if self.match("=") else TokenType.BANG)
+            case "=":
+                self.addToken(TokenType.EQUAL_EQUAL if self.match("=") else TokenType.EQUAL)
+            case "<":
+                self.addToken(TokenType.LESS_EQUAL if self.match("=") else TokenType.LESS)
+            case ">":
+                self.addToken(TokenType.GREATER_EQUAL if self.match("=") else TokenType.GREATER)
 
-        else:
-            self.error_handler(self.line, "Unexpected character.")
+            # Slashes and Comments
+            case "/":
+                if self.match("/"):
+                    # A comment goes until the end of the line
+                    while self.peek() != "\n" and not self.isAtEnd():
+                        self.advance()
+                else:
+                    self.addToken(TokenType.SLASH)
+
+            # Whitespace
+            case " " | "\r" | "\t":
+                pass
+            case "\n":
+                self.line += 1
+
+            # Default case (Unexpected characters)
+            case _:
+                self.error_handler(self.line, "Unexpected character.")
 
     def match(self, expected: str) -> bool:
         if self.isAtEnd():
@@ -88,3 +87,9 @@ class Scanner:
     def isAtEnd(self) -> bool:
         return self.current >=len(self.source)
     
+    def peek(self) -> str:
+        if self.isAtEnd():
+            return "\0"
+        return self.source[self.current]
+    
+        
