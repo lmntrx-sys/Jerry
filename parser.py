859a774d29a50548d4da829ec2d6d1d0431d9e13
infinitie <infinitiecity@gmail.com>
2026-04-18 15:56:36 +0300

    addition of missing methods

--- parser.py
+++ parser.py
@@ -12,26 +12,65 @@ class Parser:
         self.tokens = tokens
         self.current = 0
         return self.expression()
-
+    
+    # Comparison -> Term ( ( ">" | ">=" | "<" | "<=" ) Term )*
     def comparison(self, expr: Expr):
-        expr = term()
+        expr = self.term()
 
         while self.match(TokenType.GREATER,TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL):
             operator = self.previous()
-            right = term()
+            right = self.term()
             expr = Binary(expr, operator, right)
 
 
         return expr
+    
+    def primary(self):
+        if self.match(TokenType.FALSE): return Literal(False)
+        if self.match(TokenType.TRUE): return Literal(True)
+        if self.match(TokenType.NIL): return Literal(None)
 
+        if self.match(TokenType.NUMBER, TokenType.STRING):
+            return Literal(self.previous().literal)
+        
+        if self.match(TokenType.LEFT_PAREN):
+            expr = self.expression()
+            self.consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
+            return Grouping(expr)
+    
+    def term(self):
+        expr = self.factor()
+
+        while self.match(TokenType.MINUS, TokenType.PLUS):
+            operator = self.previous()
+            right = self.factor()
+            expr = Binary(expr, operator, right)
+        return expr
+    
+    def factor(self):
+        expr = self.unary()
+
+        while self.match(TokenType.SLASH, TokenType.STAR):
+            operator = self.previous()
+            right = self.unary()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def unary(self):
+        if self.match(TokenType.BANG, TokenType.MINUS):
+            operator = self.previous()
+            right = self.unary()
+            return Unary(operator, right)
+        return self.primary()
     
     # Equality -> Comparison ( "!=" | "==" ) Comparison
     def eqaulity(self):
-        expr = comparison()
+        expr = self.comparison()
 
         while self.match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL):
             operator = self.previous()
-            right = comparison()
+            right = self.comparison()
             expr = Binary(expr, operator, right)
         
         return expr
