fce105e4658542c60caeb6e96a787978d59bbc4a
infinitie <infinitiecity@gmail.com>
2026-04-30 12:46:00 +0300

    Bug fixes: Token change, removal of string, int/float concatenation: raise runtimeError, handling of divisions by zero.

--- interpreter.py
+++ interpreter.py
@@ -16,7 +16,7 @@ class Interpreter:
         return self.evaluate(node.expression)
     
     def evaluate(self, expr: Expr):
-        return expr.accept()
+        return expr.accept(self)
     
     def visitUnaryExpr(self, node: Unary) -> Any:
         """Visit the Unary node expression and return its value"""
@@ -40,8 +40,13 @@ class Interpreter:
         match node.operator.type:
             case Tk.MINUS:
                 return float(left) - float(right)
+            
+            # Handling division by zero
             case Tk.SLASH:
-                return float(left) / float(right)
+                if right == 0:
+                    self.error(Tk.SLASH, message="division by zero")
+                else:
+                    return float(left) / float(right)
             case Tk.STAR:
                 return float(left) * float(right)
             case Tk.GREATER:
@@ -51,14 +56,13 @@ class Interpreter:
             case Tk.LESS:
                 return float(left) < float(right)
             case Tk.LESS_EQUAL:
-                return float(left) >= float(right)
+                return float(left) <= float(right)
             case Tk.BANG_EQUAL:
                 return not self.isEqual(left, right)
             case Tk.EQUAL_EQUAL:
                 return self.isEqual(left, right)
             
             # We are handling a case when a user either wants to perform simple addition or concatenation
-            # We also want to hadle cases where these occur: "scone" + 4 -> "scone4"
             case Tk.PLUS:
                 if isinstance(left, (float, int)) and isinstance(right, (float, int)):
                     return float(left) + float(right)
@@ -66,13 +70,16 @@ class Interpreter:
                 if isinstance(left, str) and isinstance(right, str):
                     return left + right
                 
-                if isinstance(left, str) and isinstance(right, (float, int)):
-                    return left + str(right)
+                raise RuntimeError(f"Operands must be two numbers or two strings.")
             
         return None
     
     def isEqual(self, left, right) -> bool:
         return left == right
+    
+    def error(self, token, message):
+        error(token, message)
+        return Exception(message)
     
     def isTruthy(self, obj: Any) -> bool:
         """Anything that is not false or nil is true"""
