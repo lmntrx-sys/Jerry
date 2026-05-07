0459f4f24769f529b63d7940b746dca0a8608a5b
infinitie <infinitiecity@gmail.com>
2026-05-07 11:08:09 +0300

    re-run of Generatest.py automatic changes

--- tool/expr.py
+++ tool/expr.py
@@ -1,43 +1,38 @@
 from dataclasses import dataclass
 from typing import Any
-from Token import Token  
 
 class Expr:
-   def accept(self, visitor):
-      pass
+      def accept(self, visitor):
+          pass
 
 @dataclass
 class Binary(Expr):
-   """Represents a binary expression in the AST, consisting of a left operand, an operator, and a right operand."""
    left: Any
    operator: Any
    right: Any
 
    def accept(self, visitor):
-      return visitor.visitBinaryExpr(self)
+      return visitor.visit_binary_expr(self)
 
 @dataclass
 class Grouping(Expr):
-   """Represents a grouping expression in the AST, which is used to group sub-expressions together."""
    expression: Any
 
    def accept(self, visitor):
-      return visitor.visitGroupingExpr(self)
+      return visitor.visit_grouping_expr(self)
 
 @dataclass
 class Literal(Expr):
-   """Represents a literal value in the AST, such as a number, string, or boolean."""
    value: Any
 
    def accept(self, visitor):
-      return visitor.visitLiteralExpr(self)
+      return visitor.visit_literal_expr(self)
 
 @dataclass
 class Unary(Expr):
-   """Represents a unary expression in the AST, consisting of an operator and a right operand."""
    operator: Any
    right: Any
 
    def accept(self, visitor):
-      return visitor.visitUnaryExpr(self)
+      return visitor.visit_unary_expr(self)
 
